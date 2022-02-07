63eea51d8ed67b01422259c5fe54fc6bf94e8a55
PRANAV RAJ <pranav.raj@daimler.com>
2022-02-08 00:08:17 +0530

    keypoint for updates

---
+++ src/RFid/pi_board/read_rfid.py
@@ -0,0 +1,14 @@
+
+
+# Read rfid from environment
+
+# start -> with function call
+# end   -> after a set timeout (30s)
+
+
+# Check in database - filter out , non relevant tags
+# Aggregate data in one collection (pref: list)
+
+## Intermediate Step (without Blockchain)
+# Compare each tags with last-config database
+
