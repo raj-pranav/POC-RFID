3eeb423d4dfd2cd51ef3b286b5507e97bc026cbb
PRANAV RAJ <pranav.raj@daimler.com>
2022-02-18 22:38:05 +0530

    Boilerplate code to read rfid froma  rdm6300

--- src/RFid/pi_board/read_rfid.py
+++ src/RFid/pi_board/read_rfid.py
@@ -1,14 +1,26 @@
 from pathlib import Path
-
+import rdm6300
+import time
+import logging
 
 # Read rfid from environment
 # start -> with function call
 # end   -> after a set timeout (30s)
 
+log = logging.getLogger("RFID.Reader")
 
-# Check in database - filter out , non relevant tags
-# Aggregate data in one collection (pref: list)
+Total_tags_found = [] # to store all RFid value during current read
+set_timeout = 30  # in second
+start_time = time.time()
 
-## Intermediate Step (without Blockchain)
-# Compare each tags with last-config database
+RFid_reader = rdm6300.Reader('/dev/ttyS0')
+
+print("Start of RFid Read  >> Please wait ...")
+while (time.time() <= (start_time+set_timeout)) :
+    card = RFid_reader.read()
+    if card:
+        print(f"[{card.value}] read card {card}")
+        Total_tags_found.append(card)
+    else:
+        print ("No Tags found !!")
 
