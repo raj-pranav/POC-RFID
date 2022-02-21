598b2ed5c3d59d4587e8e6883c96a4e1ec47cf71
raj-pranav <pranav_raj1990@hotmail.com>
2022-02-22 00:55:49 +0530

    rfid interface implemented v1

--- src/RFid/pi_board/read_rfid.py
+++ src/RFid/pi_board/read_rfid.py
@@ -1,26 +1,27 @@
-from pathlib import Path
+
 import rdm6300
 import time
-import logging
 
 # Read rfid from environment
 # start -> with function call
 # end   -> after a set timeout (30s)
 
-log = logging.getLogger("RFID.Reader")
+# log = logging.getLogger("RFID.Reader")
 
 Total_tags_found = [] # to store all RFid value during current read
-set_timeout = 30  # in second
+set_timeout = 20  # in second
 start_time = time.time()
 
 RFid_reader = rdm6300.Reader('/dev/ttyS0')
 
-print("Start of RFid Read  >> Please wait ...")
+print(f"Start of RFid Read for [{set_timeout}s]... ")
 while (time.time() <= (start_time+set_timeout)) :
-    card = RFid_reader.read()
-    if card:
-        print(f"[{card.value}] read card {card}")
-        Total_tags_found.append(card)
-    else:
-        print ("No Tags found !!")
+    curr_card = RFid_reader.read()
+    Tag_id = curr_card.value
+    
+    if Tag_id not in Total_tags_found:
+        Total_tags_found.append(Tag_id)
+        print (f"Current Tag id: {Tag_id}")
+
+print (f"Total {len(Total_tags_found)} Tags found >> {Total_tags_found}")
 
