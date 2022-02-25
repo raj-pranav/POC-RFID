58840166f2163f63ef1e74b6f31b8db944dddceb
PRANAV RAJ <pranav.raj@daimler.com>
2022-02-26 00:42:15 +0530

    modified with gpiozero

--- src/RFid/pi_board/buzzer.py
+++ src/RFid/pi_board/buzzer.py
@@ -1,18 +1,36 @@
 
-import RPi.GPIO as GPIO
+import gpiozero as GPIO
 import time
 
-buzzerPin = 32  # GPIO pin: 12
+buzzerPin = 12 # BCM pin ref
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+bz = GPIO.Buzzer(buzzerPin, active_high=True, initial_value=False)
+bz.beep(on_time = 1, off_time = 1, n = 4)
 
-GPIO.setup(buzzerPin, GPIO.OUT)
-pi_pwm = GPIO.PWM(buzzerPin, 1000)  # pin with frequency
-pi_pwm.start(0)
 
-# set a pwm between 0 - 100
-duty_cyc = 10
-pi_pwm.ChangeDutyCycle(duty_cyc)
-time.sleep(2)
-pi_pwm.ChangeDutyCycle(0)
+
+"""
+-----------------------------------------------
+IMPLEMENTATION using RPi.GPIO
+-----------------------------------------------
+
+# import RPi.GPIO as GPIO
+# import time
+
+# buzzerPin = 32  # GPIO pin: 12
+
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+
+# GPIO.setup(buzzerPin, GPIO.OUT)
+# pi_pwm = GPIO.PWM(buzzerPin, 1000)  # pin with frequency
+# pi_pwm.start(0) # Start with this duty cycle
+
+# # set a pwm between 0 - 100
+# duty_cyc = 10
+# pi_pwm.ChangeDutyCycle(duty_cyc)
+# time.sleep(2)
+# pi_pwm.stop()
+
+"""
+
